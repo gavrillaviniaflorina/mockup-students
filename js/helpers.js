e36106b51b84aa959464f89a05d8a04722dce896
Lavinia Gavril <lavinia.florina.gavril@gmail.com>
2022-01-19 15:01:43 +0200

    second

--- js/helpers.js
+++ js/helpers.js
@@ -3,16 +3,62 @@
 let setHome = () => {
 
 
-    appendCardsPerPag(1);
+    appendCardsPerPag(1, data);
 
-
+    attatchButtons(calculateButtons());
 
     window.addEventListener("resize", () => {
 
-        appendCardsPerPag(1);
+        appendCardsPerPag(1, data);
+        attatchButtons(calculateButtons());
     })
 
-    selectPage();
+
+    let buttons = document.querySelector(".buttons");
+
+    buttons.addEventListener('click', (e) => {
+        let obj = e.target;
+
+        if (obj.tagName == "BUTTON") {
+            let val = obj.textContent;
+            appendCardsPerPag(val, data);
+            obj.classList.add("selected");
+
+        }
+    })
+
+
+    //search();
+
+
+    let input = document.querySelector(".searching");
+
+    input.addEventListener("input", (e) => {
+
+
+        let text = input.value;
+
+
+        let arr = searchingName(text);
+
+
+        if (arr.length > 0) {
+            appendCardsPerPag(1, [...arr]);
+        } else {
+            let students = document.querySelector(".students");
+            students.innerHTML = `<setion>No results</section>`;
+        }
+
+
+
+    })
+
+
+    createOver();
+
+
+
+
 
 }
 
@@ -55,31 +101,128 @@ let appendCards = (arr) => {
         students.append(card);
 
 
-        // console.log(students);
+
 
     }
 }
 
+let createOver = () => {
+    let students = document.querySelector(".students");
+    let body = document.querySelector(".body");
+    let over = document.createElement("section");
+
+
+    students.addEventListener("click", (e) => {
+        let obj = e.target;
+
+
+        if (obj.tagName == "IMG") {
+
+
+
+            over.classList = "over";
+
+
+
+            let result = document.createElement("article");
+            result.classList = "student1";
+            let parent = obj.parentNode;
+            result.innerHTML = `${parent.innerHTML}`;
+
+            let left = document.createElement("i");
+            left.classList = "fas";
+            left.classList.add("fa-arrow-left");
+
+            result.appendChild(left);
+
+
+            let right = document.createElement("i");
+            right.classList = "fas";
+            right.classList.add("fa-arrow-right");
+
+            result.appendChild(right);
+
+
+
+            over.appendChild(result);
+            body.appendChild(over);
+            arrowRight();
+
+
+        }
+
+
+    })
+
+
+    // over.addEventListener(("click"), (e) => {
+
+
+
+    //     over.removeChild(over.firstElementChild);
+    //     body.removeChild(over);
+    // })
+
+
+
+}
+
+
+
+let arrowRight = () => {
+    let right = document.querySelector(".fa-arrow-right");
+    // let student1 = document.querySelector("student1");
+    let over = document.querySelector(".over");
+    let student1 = over.firstElementChild;
+
+    right.addEventListener("click", (e) => {
+
+        let email = student1.firstElementChild.nextElementSibling.nextElementSibling.textContent;
+        console.log(email);
+        let a = searchNext(email);
+        console.log(a);
+        over.removeChild(over.firstElementChild);
+        let card = createCard(searchNext(email));
+
+
+        card.classList = "student1";
+
+        let left = document.createElement("i");
+        left.classList = "fas";
+        left.classList.add("fa-arrow-left");
+
+        let right = document.createElement("i");
+        right.classList = "fas";
+        right.classList.add("fa-arrow-right");
+
+        card.append(left);
+        card.append(right);
+
+        over.appendChild(card);
+
+
+    })
+}
+
+
+
+
+
 
 //functie ce primeste ca parametru nr paginii si ataseaza carduri
 
-let appendCardsPerPag = (nrPag) => {
+let appendCardsPerPag = (nrPag, arr) => {
 
     let cards = [];
     if (window.innerWidth < 720) {
-        cards = cardsPerPag(nrPag, 5, data);
+        cards = cardsPerPag(nrPag, 5, arr);
     } else if (window.innerWidth < 1024) {
-        cards = cardsPerPag(nrPag, 6, data);
+        cards = cardsPerPag(nrPag, 6, arr);
     } else {
-        cards = cardsPerPag(nrPag, 9, data);
+        cards = cardsPerPag(nrPag, 9, arr);
     }
 
     appendCards(cards);
-
-    attatchButtons(calculateButtons());
-
-
-
 
 
 }
@@ -115,15 +258,13 @@ let attatchButtons = (nr) => {
     buttons.innerHTML = '';
     for (let i = 1; i <= nr; i++) {
         let button = document.createElement("button");
-        button.classList = "button";
+        button.classList.add("button");
         button.innerHTML = `${i}`;
         buttons.append(button);
-
-
-
     }
 
-    //console.log(buttons);
+
+
 
 }
 
@@ -141,67 +282,34 @@ let calculateButtons = () => {
     }
 }
 
-let selectPage = () => {
 
-    let buttons = document.querySelector(".buttons");
-    buttons.addEventListener('click', (e) => {
-        let obj = e.target;
+let searchingName = (name) => {
+    let arr = data;
+    let results = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name.first.includes(name) || arr[i].name.last.includes(name)) {
 
-        if (obj.tagName == "BUTTON") {
-
-            obj.classList.add("selected");
-
-            let value = obj.textContent;
-
-            appendCardsPerPag(value);
-
-
-
-
-            console.log(obj);
-            return obj;
-
+            results.push(arr[i]);
         }
-    })
+    }
 
 
-
-
+    return results;
 
 }
 
-let searchingName = (name, arr) => {
+let searchNext = (mail) => {
+
+    let arr = data;
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].name.first == name) {
+        if (arr[i].email.includes(mail)) {
+            console.log(arr[i + 1]);
+
+            break;
 
 
-            return arr[i];
         }
-
+        return arr[i + 1];
     }
-}
-
-let search = () => {
-    let submit = document.querySelector(".submit");
-    let searching = document.querySelector(".searching");
-
-    submit.addEventListener('click', (e) => {
-        let obj = e.target;
-
-        if (obj.tagName == "BUTTON") {
-
-            obj.classList.add("selected");
-
-            let value = obj.textContent;
-
-            appendCardsPerPag(value);
-
-
-
-
-            console.log(obj);
-        }
-
-    })
 }
